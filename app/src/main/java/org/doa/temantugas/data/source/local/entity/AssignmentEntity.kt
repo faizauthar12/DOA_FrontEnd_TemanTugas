c44829ce39b42d5c4cd739d44b0a2822cb82563c
Faiz Authar <faizauthar@gmail.com>
2021-07-17 19:49:47 +0700

    TemanTugas: Assignment RecycleView[1/6]: Initial Assignment Entity

---
+++ app/src/main/java/org/doa/temantugas/data/source/local/entity/AssignmentEntity.kt
@@ -0,0 +1,8 @@
+package org.doa.temantugas.data.source.local.entity
+
+data class AssignmentEntity(
+    var assignmentId: String?,
+    var courseId: String?,
+    var assignmentTitle: String?,
+    var assignmentDeadline: String?,
+)
