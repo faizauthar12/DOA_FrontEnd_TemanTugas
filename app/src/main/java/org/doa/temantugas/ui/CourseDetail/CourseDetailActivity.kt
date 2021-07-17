560a80f52608c05391806af3b644c66333b44eaf
Faiz Authar <faizauthar@gmail.com>
2021-07-17 17:09:05 +0700

    TemanTugas: CourseDetail BottomNav[2/2]: Implement logic

--- app/src/main/java/org/doa/temantugas/ui/CourseDetail/CourseDetailActivity.kt
+++ app/src/main/java/org/doa/temantugas/ui/CourseDetail/CourseDetailActivity.kt
@@ -2,6 +2,11 @@ package org.doa.temantugas.ui.CourseDetail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import org.doa.temantugas.R
 import org.doa.temantugas.databinding.ActivityCourseDetailBinding
 
 class CourseDetailActivity : AppCompatActivity() {
@@ -12,5 +17,18 @@ class CourseDetailActivity : AppCompatActivity() {
 
         binding = ActivityCourseDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupBottomNavigation()
+    }
+
+    private fun setupBottomNavigation() {
+        val navView: BottomNavigationView = binding.bottomNavigationViewCourseDetail
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerViewCourseDetail) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+        navView.setupWithNavController(navController)
     }
 }
