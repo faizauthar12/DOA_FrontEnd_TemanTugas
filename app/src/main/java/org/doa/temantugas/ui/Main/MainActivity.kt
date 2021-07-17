b8c371710d94c011edfda7530f68abf4f37030b2
Faiz Authar <faizauthar@gmail.com>
2021-07-17 10:45:31 +0700

    TemanTugas: Main BottomNav[2/2]: bottom navigation logic

--- app/src/main/java/org/doa/temantugas/ui/Main/MainActivity.kt
+++ app/src/main/java/org/doa/temantugas/ui/Main/MainActivity.kt
@@ -2,11 +2,34 @@ package org.doa.temantugas.ui.Main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import org.doa.temantugas.R
+import org.doa.temantugas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        setupBottomNavigation(activityMainBinding)
+
+        supportActionBar?.hide()
+    }
+
+    private fun setupBottomNavigation(activityMainBinding: ActivityMainBinding) {
+        val navView: BottomNavigationView = activityMainBinding.bottomNavigationView
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+        navView.setupWithNavController(navController)
     }
 }
