7c2fe4f58ec3010c2f5520f6b43fc3e4e7217e8b
Faiz Authar <faizauthar@gmail.com>
2021-07-17 09:13:11 +0700

    TemanTugas: Initial barebone code

---
+++ app/src/main/java/org/doa/temantugas/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.doa.temantugas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
