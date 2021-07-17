ccc3f7f9517ef9332146f435b40cdc22de88c507
Faiz Authar <faizauthar@gmail.com>
2021-07-17 11:54:58 +0700

    TemanTugas: CleanUp Home,Calendar,Me Fragment

--- app/src/main/java/org/doa/temantugas/ui/Me/MeFragment.kt
+++ app/src/main/java/org/doa/temantugas/ui/Me/MeFragment.kt
@@ -8,10 +8,6 @@ import android.view.ViewGroup
 import org.doa.temantugas.R
 
 class MeFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
