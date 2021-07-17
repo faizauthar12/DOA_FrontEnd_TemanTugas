b07918d5ad7984a0e3e9d15b8f40d74c4be4d6b4
Faiz Authar <faizauthar@gmail.com>
2021-07-17 11:59:43 +0700

    TemanTugas: Home,Calendar,Me Add jetpack binding

--- app/src/main/java/org/doa/temantugas/ui/Home/HomeFragment.kt
+++ app/src/main/java/org/doa/temantugas/ui/Home/HomeFragment.kt
@@ -5,14 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import org.doa.temantugas.R
+import org.doa.temantugas.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 }
--- app/src/main/java/org/doa/temantugas/ui/Me/MeFragment.kt
+++ app/src/main/java/org/doa/temantugas/ui/Me/MeFragment.kt
@@ -5,14 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import org.doa.temantugas.R
+import org.doa.temantugas.databinding.FragmentMeBinding
 
 class MeFragment : Fragment() {
+    private lateinit var binding: FragmentMeBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_me, container, false)
+        binding = FragmentMeBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 }
