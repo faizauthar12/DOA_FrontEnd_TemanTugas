348b506fc57f6fde80a5b407a9b38c0424f5d765
Faiz Authar <faizauthar@gmail.com>
2021-07-18 11:07:00 +0700

    TemanTugas: CourseDetailActivity: SetSupportActionBar

--- app/src/main/java/org/doa/temantugas/ui/CourseDetail/CourseDetailActivity.kt
+++ app/src/main/java/org/doa/temantugas/ui/CourseDetail/CourseDetailActivity.kt
@@ -1,7 +1,7 @@
 package org.doa.temantugas.ui.CourseDetail
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -22,6 +22,7 @@ class CourseDetailActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setupBottomNavigation()
+        setSupportActionBar(binding.toolbarCourseDetail)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         if (intent.extras != null) {
