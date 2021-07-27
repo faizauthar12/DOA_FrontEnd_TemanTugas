dc2f5a8fc815b2533098cbce1a19565b5ce78d10
Faiz Authar <faizauthar@gmail.com>
2021-07-27 11:50:14 +0700

    TemanTugas: Assignment RecycleView[6/6]: Implement into the AssignmentFragment

--- app/src/main/java/org/doa/temantugas/ui/Assignment/AssignmentFragment.kt
+++ app/src/main/java/org/doa/temantugas/ui/Assignment/AssignmentFragment.kt
@@ -5,10 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import org.doa.temantugas.databinding.FragmentAssignmentBinding
+import org.doa.temantugas.ui.CourseDetail.CourseDetailSharedViewModel
 
 class AssignmentFragment : Fragment() {
     private lateinit var binding: FragmentAssignmentBinding
+    private val viewModel: CourseDetailSharedViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,5 +21,22 @@ class AssignmentFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentAssignmentBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (activity != null) {
+            val assignments = viewModel.getAssignments()
+
+            val assignmentAdapter = AssignmentAdapter()
+            assignmentAdapter.setAssignment(assignments)
+
+            with(binding.rvAssignment) {
+                layoutManager = LinearLayoutManager(context)
+                setHasFixedSize(true)
+                adapter = assignmentAdapter
+            }
+        }
     }
 }
