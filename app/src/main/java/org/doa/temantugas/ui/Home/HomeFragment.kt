bee4ca36224585bfdf87be3fb125b25aae8c21f9
Faiz Authar <faizauthar@gmail.com>
2021-07-27 10:30:07 +0700

    TemanTugas: HomeFragment: Initial dummy code

--- app/src/main/java/org/doa/temantugas/ui/Home/HomeFragment.kt
+++ app/src/main/java/org/doa/temantugas/ui/Home/HomeFragment.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import org.doa.temantugas.R
 import org.doa.temantugas.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -17,5 +20,27 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (activity != null) {
+            /* Avatar Glide*/
+            Glide.with(this)
+                .load(R.drawable.avatar_dummy)
+                .centerCrop()
+                .override(52)
+                .transform(RoundedCorners(20))
+                .into(binding.ivProfileHome)
+
+            /* Dummy Percentage Glide */
+            Glide.with(this)
+                .load(R.drawable.percentage_dummy)
+                .into(binding.ivPercentageHome)
+
+            /* Set percentage title */
+            binding.tvPrecentageTitle.text = "Presentase Tugas Mingguan"
+        }
     }
 }
